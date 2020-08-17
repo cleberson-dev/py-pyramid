1cca88f21dfbaebdd754341d1428188fad5fcdb5
Cleberson F R Junior <clebersondev@pop-os.localdomain>
2020-08-17 18:05:11 -0300

    Created POST '/cars' route handler

--- src/views.py
+++ src/views.py
@@ -16,3 +16,19 @@ cars = [
 )
 def get_cars(request):
   return cars
+
+@view_config(
+  route_name='cars',
+  renderer='json',
+  request_method='POST'
+)
+def create_car(request):
+  body = request.json_body
+  new_car = { 
+    "id": str(len(cars)),
+    "make": body['make'],
+    "model": body['model']
+   }
+  cars.append(new_car)
+  return cars
+  
